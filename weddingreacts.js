927eda0786e16c88a16f259cdb8c681e60b0eb34
PDavid6 <62878435+PDavid6@users.noreply.github.com>
2020-04-28 19:22:24 +0200

    refreshing javafunctions and html frame

--- weddingreacts.js
+++ weddingreacts.js
@@ -4,7 +4,21 @@ let programs = document.getElementById('progs');
 let gifts = document.getElementById('present');
 let faq = document.getElementById('faq');
 let contacts = document.getElementById('contact');
+let topButton = document.getElementById('topbuttoncontainer');
 
+
+function scrollDisplay () {
+    if (document.documentElement.scrollTop > 20 ){
+    topButton.style.display = "block"
+} else {
+    topButton.style.display = "none"
+}
+}
+window.onscroll = function() {scrollDisplay()};
+
+function scrollToTop () {
+    document.documentElement.scrollTop = 0;
+}
 
 function scrollToMain () {
 mainpage.scrollIntoView({behavior: "smooth", block: "center"});
@@ -31,4 +45,5 @@ document.getElementById('progb').addEventListener('click', scrollToPrograms );
 document.getElementById('giftb').addEventListener('click', scrollToGifts );
 document.getElementById('faqb').addEventListener('click', scrollToFaq );
 document.getElementById('contb').addEventListener('click', scrollToContacts );
+document.getElementById('backtotop').addEventListener('click', scrollToTop)
 
